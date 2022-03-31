225e1ff34bd8195b7180d185b07a46c0369d960b
georgecatalin <george.calin@gmail.com>
2022-03-31 13:32:48 +0300

    Using numeric operators in Python

---
+++ Stepping into the World of Python/HelloWorld/numeric_operators.py
@@ -0,0 +1,13 @@
+a = 12
+b = 3
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)    # 4.0
+print(a // b)   # 4 ; this is integer division
+print(a % b)    # 0 ; modulus b
+
+print()
+for i in range(1 , 4):
+    print(i)
