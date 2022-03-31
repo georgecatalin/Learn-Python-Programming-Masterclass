94f6eae24b3cd758a9a17551b46ae714f4e9857c
georgecatalin <george.calin@gmail.com>
2022-03-31 14:28:12 +0300

    negative indexing in strings

---
+++ Stepping into the World of Python/HelloWorld/negative_indexing.py
@@ -0,0 +1,20 @@
+parrot = "Norwegian Blue"
+
+# this is negative indexing: starts from the end of the string
+print(parrot[-1])  # e
+print(parrot[-2])  # u
+
+print(parrot[3])    # -11
+print(parrot[4])    # -10
+print(parrot[9])    # -5
+print(parrot[3])    # -11
+print(parrot[6])    # -8
+print(parrot[8])  # -6
+
+print("********************")
+print(parrot[-11])
+print(parrot[-10])
+print(parrot[-5])
+print(parrot[-11])
+print(parrot[-8])
+print(parrot[-6])
