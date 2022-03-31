85483fc3e313d2b57d105aa1a49cacb8ca0515db
georgecatalin <george.calin@gmail.com>
2022-03-31 14:19:04 +0300

    The str String Data Type

---
+++ Stepping into the World of Python/HelloWorld/str_string_data_type.py
@@ -0,0 +1,12 @@
+parrot = "Norwegian Blue"
+
+print(type(parrot))
+print(parrot)
+print(parrot[3])
+print()
+print(parrot[3])
+print(parrot[4])
+print()
+print(parrot[3])
+print(parrot[6])
+print(parrot[8])
