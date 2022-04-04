c46fc8e3cda67aa4a2cc701be745ea8834a76623
georgecatalin <george.calin@gmail.com>
2022-04-04 15:45:35 +0300

    using f-strings in Python version more than 3.6 including

---
+++ Stepping into the World of Python/HelloWorld/f-strings.py
@@ -0,0 +1,9 @@
+# f-strings is a method for formatting strings introduced in Python 3.6 . Earlier version will not support it
+
+my_age = 43
+print(f"My age is {my_age} years old.")
+
+print(f"The value of PI is approximately {22 / 7:12.50f}")
+
+pi = 22 / 7
+print(f"The value of PI is approximately {pi:72.50f}")
