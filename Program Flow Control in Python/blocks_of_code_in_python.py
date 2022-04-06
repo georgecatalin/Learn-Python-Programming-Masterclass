4ab40d366e3324ce6efdb1209cbe75cbf9de84a4
georgecatalin <george.calin@gmail.com>
2022-04-06 14:56:20 +0300

    using blocks of code and indentation in Python

---
+++ Program Flow Control in Python/blocks_of_code_in_python.py
@@ -0,0 +1,11 @@
+for i in range(1, 20):
+    print("Number {} had a square of {} and a cube of {}".format(i, i ** 2, i ** 3))
+print("*" * 60)
+
+print()
+
+for i in range(1, 15):
+    print("Number {} had a square of {} and a cube of {}".format(i, i ** 2, i ** 3))
+    print("*" * 60)
+
+    
