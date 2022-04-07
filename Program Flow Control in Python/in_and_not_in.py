3bad583eb0131bef70d18f24ec43bfed2c125619
georgecatalin <george.calin@gmail.com>
2022-04-07 11:58:40 +0300

    in and not in in Python

---
+++ Program Flow Control in Python/in_and_not_in.py
@@ -0,0 +1,6 @@
+answer = input("What do you plan to do today, Alligator? ")
+
+if "cinema" not in answer.casefold():
+    print("Fuck, I planned to go to the cinema today.")
+else:
+    print("Great thing. Respect, bro")
