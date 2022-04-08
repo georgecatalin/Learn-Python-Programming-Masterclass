b549061f6d5b5d65f0ce4fc924bfc23679028f4d
georgecatalin <george.calin@gmail.com>
2022-04-08 14:24:28 +0300

    continue in Python

---
+++ Program Flow Control in Python/continue_in_Python.py
@@ -0,0 +1,10 @@
+shopping_list = ["coca-cola", "pepsi", "sprite", "fanta", "mirinda"]
+
+for item in shopping_list:
+    # if item != "pepsi":
+    #     print(item)
+
+    if item == "pepsi":
+        continue
+
+    print(item)
