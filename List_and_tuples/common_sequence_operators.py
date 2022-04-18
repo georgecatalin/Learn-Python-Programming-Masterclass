6d85fe2ff04e00d26721083038a0166e74b380f9
georgecatalin <george.calin@gmail.com>
2022-04-18 10:35:34 +0300

    common sequence operators

---
+++ List_and_tuples/common_sequence_operators.py
@@ -0,0 +1,16 @@
+odd = [1, 3, 5, 7, 9]
+even = [0, 2, 4, 6, 8]
+
+print(min(odd))
+print(max(odd))
+
+print("_" * 50)
+print(min(even))
+print(max(even))
+
+print("_" * 50)
+print(len(odd))
+print(len(even))
+
+print("_" * 50)
+print("mississipi".count("s"))
