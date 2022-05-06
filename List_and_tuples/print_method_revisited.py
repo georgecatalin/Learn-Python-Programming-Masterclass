4157ea427e7659a8be25818afc749b9279bedddf
georgecatalin <george.calin@gmail.com>
2022-05-06 15:06:57 +0300

    revisiting the print method in python

---
+++ List_and_tuples/print_method_revisited.py
@@ -0,0 +1,8 @@
+name = "George"
+age = 43
+year = 1978
+
+print(name, age, year, "happy")
+print(name,age,year, "happy" ,sep="; ")
+print(name, age, year, "happy", end=" \t")
+print(name, age, year, "happy")
