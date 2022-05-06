fea4bf0c258d15a815637eac2f21f9144d50818d
georgecatalin <george.calin@gmail.com>
2022-05-06 15:29:36 +0300

    using the join method

---
+++ List_and_tuples/using_the_join_method.py
@@ -0,0 +1,17 @@
+my_list = [
+    "Audi",
+    "Skoda",
+    "VW",
+    "Renault",
+    "Opel",
+    "Volvo",
+    "Nissan",
+]
+
+# for item in my_list:
+#    print(item)
+
+separator = " | "
+print(separator.join(my_list))
+
+print("; ".join(my_list))
