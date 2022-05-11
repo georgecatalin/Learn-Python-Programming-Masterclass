801ffc58eb0c9e4a7fb1c0e5e8bb4ac8f8d25639
georgecatalin <george.calin@gmail.com>
2022-05-11 16:52:05 +0300

    using nested indexing in Python

---
+++ List_and_tuples/nested_indexing.py
@@ -0,0 +1,62 @@
+albums = [
+    ("Welcome to my Nightmare", "Alice Cooper", 1975,
+     [
+         (1, "Welcome to my Nightmare"),
+         (2, "Devil's Food"),
+         (3, "The Black Widow"),
+         (4, "Some Folks"),
+         (5, "Only Women Bleed"),
+     ]
+     ),
+    ("Bad Company", "Bad Company", 1974,
+     [
+         (1, "Can't Get Enough"),
+         (2, "Rock Steady"),
+         (3, "Ready for Love"),
+         (4, "Don't Let Me Down"),
+         (5, "Bad Company"),
+         (6, "The Way I Choose"),
+         (7, "Movin' On"),
+         (8, "Seagull"),
+     ]
+     ),
+    ("Nightflight", "Budgie", 1981,
+     [
+         (1, "I Turned to Stone"),
+         (2, "Keeping a Rendezvous"),
+         (3, "Reaper of the Glory"),
+         (4, "She Used Me Up"),
+     ]
+     ),
+    ("More Mayhem", "Imelda May", 2011,
+     [
+         (1, "Pulling the Rug"),
+         (2, "Psycho"),
+         (3, "Mayhem"),
+         (4, "Kentish Town Waltz"),
+     ]
+     ),
+]
+
+
+album = albums[3]
+print(album)
+
+songs = album[3]
+print(songs)
+
+song = songs[3]
+print(song)
+
+title =song[1]
+print(title)
+
+# all indexes at once
+my_title= albums[3][3][3][1]
+print(my_title)
+
+# applying indexes step by step
+print(albums[3])
+print(albums[3][3])
+print(albums[3][3][3])
+print(albums[3][3][3][1])
