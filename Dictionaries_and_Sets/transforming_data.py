a9eb6860c7fa70599724f6bdd11ab8c8db226b09
georgecatalin <george.calin@gmail.com>
2022-05-19 16:34:17 +0300

    transforming data with Python

---
+++ Dictionaries_and_Sets/transforming_data.py
@@ -0,0 +1,27 @@
+from contents import pantry, recipes
+
+
+# print the dictionaries to demonstrate they were imported properly
+
+# print(pantry)
+# print(recipes)
+
+
+display_dictionary = {}
+for index, key in enumerate(recipes):
+    # print(f"{index}: {key}")
+    display_dictionary[str(index+1)] = key
+
+# print(display_dictionary)
+
+while True:
+    print("Choose from the menu:")
+    print("=====================")
+    choice = input("> ")
+
+    for key, value in display_dictionary.items():
+        print(f"{key} - {value}")
+
+    if choice == 0:
+        break
+
